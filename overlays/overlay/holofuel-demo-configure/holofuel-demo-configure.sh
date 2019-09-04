984d57d9760cf6932292f6047d5d657a4cc80fcd
Perry Kundert <perry@hardconsulting.com>
2019-09-04 17:33:28 -0600

    Update holoportos-initialize and holofuel-demo-configure o UI not yet installed to /var/lib/holo-envoy/.holochain/holo/ui-store/

--- overlays/overlay/holofuel-demo-configure/holofuel-demo-configure.sh
+++ overlays/overlay/holofuel-demo-configure/holofuel-demo-configure.sh
@@ -18,17 +18,20 @@ if [ "$(whoami)" != "root" ]; then
   exit 1
 fi
 
-PUBKEY=$( cat /var/lib/holochain-conductor/holoportos-key.pub )
-if (( $? )); then
-  echo "HoloFuel Demo configuration needs you to run holoportos-initialize (eg. generate agent key) first."
-  exit 1
+KEY=/var/lib/holochain-conductor/holoportos-key
+
+if ! PUBKEY=$( cat ${KEY}.pub ); then
+  echo "HoloFuel Demo runng holoportos-initialize (eg. generate agent key) first."
+  systemctl stop holochain-conductor.service # stop, for memory and b/c it requires key to run
+  holoportos-initialize -v ${KEY} || ( echo "HoloFuel Demo failed to initialize Agent key"; exit 1 )
+  systemctl start holochain-conductor.service
+  PUBKEY=$( cat ${KEY}.pub )
 fi
-echo "Host Agent ID: ${PUBKEY}"
+echo "Host Agent ID:        ${PUBKEY}"
 
 # OK, we're root, and we've got an Agent ID key.  Lets wait 'til holochain-conductor is up
-echo -n "The systemd holochain-conductor.service: "
-systemctl is-active holochain-conductor.service
-if (( $? )); then 
+echo -n "Holochain Conductor:  "
+if ! systemctl is-active holochain-conductor.service; then
     echo "HoloFuel Demo configuration need holochain-conductor.service to be active."
     exit 1
 fi
@@ -36,17 +39,17 @@ fi
 # But, we have to wait 'til Envoy connects.  The service runs, but we need to see "All connections
 # established!", indicating that it has begun communicating with the Holochain conductor (just since
 # the last boot)
-echo -n "Awaiting holo-envoy ."
+echo -n "Awaiting holo-envoy..."
 while ! journalctl -u holo-envoy.service -b | grep "All connections established!"; do
     sleep 5
     echo -n .
 done
-echo "Host Envoy:   Running"
+echo "Running"
 
 # Envoy is up and running.  Let's Get Ready to Rumble!
 
 echo -n "Register as Provider and Host... "
-if holo provider register ${EMAIL}
+if holo provider register ${EMAIL} \
   && holo host   register ${EMAIL}; then
     echo "Successfully registered"
 else
@@ -54,48 +57,55 @@ else
     exit 1
 fi
 
-echo -n "Creating 'holofuel' hApp... "
+echo -n "The 'holofuel' hApp... "
 if ! holo happ create holofuel ${HOLOFUEL_DNA_FILE} ${HOLOFUEL_DNA_HASH}; then
     echo "Failed to create holoful hApp"
     exit 1
 fi
-echo "Successful; List of hApps:"
+echo "Created Successfully; List of hApps:"
 holo happ list
 
 # TODO: Get hApp hash from holo happ create
 HOLOFUEL_HAPPSTORE_HASH=QmT9sisxtTXKGinCjcxp5nd1JhnbZDPru4sYJYXNSpM8U4
 HOLOFUEL_HAPPPROVI_HASH=QmYF8vySWg1UEmDP2zLHBbCg5VZgMe1KcmDAgTiFNmmspJ
 
-echo -n "Registering to provide 'holofuel' hApp... "
-if ! holo provider register-app ${HOLOFUEL_HAPPSTORE_HASH} ${HOLOFUEL_DEMO_SITE}
+echo -n "Provider registering..."
+if ! holo provider register-app ${HOLOFUEL_HAPPSTORE_HASH} ${HOLOFUEL_DEMO_SITE}; then
     echo "Failed to provision holoful hApp"
     exit 1
 fi
-echo "Successful"
+echo "Successfully provisioning 'holofuel' hApp"
 
-echo -n "Enable Hosting of the 'holofuel' hApp... "
-if ! holo host enable ${HOLOFUEL_HAPPPROVI_HASH}
+echo -n "Enabling Hosting...    "
+if ! holo host enable ${HOLOFUEL_HAPPPROVI_HASH}; then
     echo "Failed to enable Hosting of the holoful hApp"
     exit 1
 fi
 echo "Successful"
 
-mkdir -p ${HOLOCHAIN_DNS_DIR}
+# TODO: install holo-envoy as a non-root user, w/ home dir...
+HOLOENVOY_HOME=/var/lib/holo-envoy
+HOLOENVOY_DNA_DIR=${HOLOENVOY_HOME}/.holochain/holo/dnas
+HOLOENVOY_UIS_DIR=${HOLOENVOY_HOME}/.holochain/holo/ui-store
 
-echo -n "Installing the 'holofuel' hApp... "
-if ! holo admin install ${HOLOFUEL_HAPPSTORE_HASH} --directory ${HOLOCHAIN_DNS_DIR}; then
+mkdir -p ${HOLOENVOY_DNA_DIR}
+mkdir -p ${HOLOENVOY_UIS_DIR}
+ln -fs   ${HOLOENVOY_HOME}/.holochain /root/ # /root/.holochain -> /var/lib/holo-envoy/.holochain
+
+echo -n "Installing the hApp... "
+if ! holo admin install ${HOLOFUEL_HAPPSTORE_HASH} --directory ${HOLOENVOY_DNA_DIR}; then
     echo "Failed to install holoful hApp "
     exit 1
 fi
-echo "Successful; Available DNAs: "
+echo "Successfully installed 'holofuel'; Available DNAs: "
 holo admin dna
 
-echo -n "Create ServiceLogger for 'holofuel' hApp... "
+echo -n "Create ServiceLogger..."
 if ! holo admin init QmT9sisxtTXKGinCjcxp5nd1JhnbZDPru4sYJYXNSpM8U4 --service-logger servicelogger; then
     echo "Failed to init holoful hApp "
     exit 1
 fi
-echo "Successful; Available instances: "
+echo "Successfully created for 'holofuel' hApp...; Available instances: "
 holo admin instance
 
 echo "Done; Available interfaces: "
