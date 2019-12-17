c240c2845c722c2608fb7ab49184bd30ecb2f26e
Perry Kundert <perry@hardconsulting.com>
2019-12-17 07:49:26 -0800

    Upgrade all DNAs to holochain-rust 0.0.40+ develop

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -3,24 +3,26 @@ final: previous:
 with final;
 
 let
+  # Not all of these DNAs are used, but are available here for manual `nix-build -A ...`
+  happ-example = fetchFromGitHub {
+    owner = "Holo-Host";
+    repo = "happ-example";
+    rev = "7bdc2731de39bd83712feeab60e82ce983bed0d6";
+    sha256 = "1q6y05dpxyb63is7p9gb385r6ixccdjrbnh7ldvxzcgn5jnb8y0i";
+  };
+
   happ-store = fetchFromGitHub {
     owner = "holochain";
     repo = "happ-store";
-    rev = "4e27b888810b45d706b2982f7d97aa454aaf74cf";
-    sha256 = "18h0x2m5vnmm1xz5k0j7rsc4il62vhq29qcl7wn1f9vmsfac2lrv";
-  };
-
-  holofuel = fetchurl {
-    url = "https://holo-host.github.io/holofuel/releases/download/v0.13.1-alpha1/holofuel.dna.json";
-    name = "holofuel.dna.json";
-    sha256 = "10xmq52m37r09flmsri64iwrpi9l88s9zmdl50l346ld6rxd0jf8";
+    rev = "1c54a210089f93b54f5678d9353edb90070784de";
+    sha256 = "0plaz9dxsy44wq4jn5yw0aqpqlf3ga5ppbz5sq29h2cgl3x23m2k";
   };
 
   holo-hosting-app = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "holo-hosting-app";
-    rev = "bbda39876d5bc206712fcbe27fdb5e405006e539";
-    sha256 = "0wf7133cam8s3m6hww5fk29343z2a0xf2qv764radx5pcr02lhs0";
+    rev = "be1e30020744cf5317e0362a288bfdfbae93dcdc";
+    sha256 = "0yjnn2iyvk5wf1rv76sqr4jxhss10zxsyf5kld99brgpv5gn16v8";
   };
 
   servicelogger = fetchFromGitHub {
@@ -29,9 +31,17 @@ let
     rev = "de10163bc9b17522a455852e1f36001f355496ff";
     sha256 = "1lvvcksibh0njj9fvl7bzvinc4vv5jkcmxsys5h3z5z5k5a9h64l";
   };
+
+  holofuel = fetchurl {
+    url = "https://holo-host.github.io/holofuel/releases/download/v0.14.0-alpha1/holofuel.dna.json";
+    name = "holofuel.dna.json";
+    sha256 = "0149vz255gyn6qkjg516gvmwhz1w4q03hn3grm5nssryd6kgasw0";
+  };
 in
 
 {
+  inherit (callPackage happ-example {}) happ-example;
+
   inherit (callPackage happ-store {}) happ-store;
 
   inherit (callPackage holo-hosting-app {}) holo-hosting-app;
