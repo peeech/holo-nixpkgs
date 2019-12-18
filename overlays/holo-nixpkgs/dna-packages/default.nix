ee272ec7bda043280d6e61b7a86239c2f9d26383
Perry Kundert <perry@hardconsulting.com>
2019-12-18 14:31:11 -0800

    Update dna-packages to latest holochain-rust

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -7,36 +7,36 @@ let
   happ-example = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "happ-example";
-    rev = "d59256a67ab1208b7597c7a3a4871b041b2f3d5c";
-    sha256 = "0a91jncsva9fxs23qwkil6bpcsq376h9vy675ksa9im8xc52k7l0";
+    rev = "69f086021af0180a8132f46f3d041884f9c43fd8";
+    sha256 = "152gmg64j1kjkc1rhxbq087bcn2qic09wh9iyghxwfgw1mkywpid";
   };
 
   happ-store = fetchFromGitHub {
     owner = "holochain";
     repo = "happ-store";
-    rev = "fe06d08f014d1d01d18f0d48f465d177b4491bc2";
-    sha256 = "1m3d9wsyrcs5rwhl7s20shxrz6zz6m39bsmv3l7h5jnjamxamlj7";
+    rev = "a3d26d6cf4c45e0d503aabd0fa5bbe7e6c6d6a74";
+    sha256 = "02s6d20nr9iwb57v0sp0sbh5pkwyara12bkv24p1ch3cg9clsq0i";
   };
 
   holo-hosting-app = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "holo-hosting-app";
-    rev = "7c1715537440fd9284f84d5337e0b8d56c690e0e";
-    sha256 = "13vpf29c631gzxa0n1mmby1cpxrimv4f02dps11s7ia9lzcx854q";
+    rev = "a84a8de8955ce32471aa758021841ff02c1f8f99";
+    sha256 = "03dbp8mh99v618p40vkwdgrzcwf6xwzqpfhjq0y3z4spnq280lk5";
   };
 
   hylo-holo-dnas = fetchFromGitHub {
     owner = "holochain";
     repo = "hylo-holo-dnas";
-    rev = "675a563204af6bc72fd3e2351e8f12bce23e7073";
-    sha256 = "1hbywvgm69hxjzkpaxx6lj2anvjqp04y00b4iz4fb8bag7sa80kx";
+    rev = "4c649ff014676c976cc7181415ddefed25ff36ad";
+    sha256 = "06v3q70ahr7m24janc2msb2jvb1mfy8xyri3k53khcwn0pkwdr8l";
   };
 
   servicelogger = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "servicelogger";
-    rev = "5417eb8e44f52bea9a56f8261af8d060d0ea7f97";
-    sha256 = "0wyym97bv6a6va9whgpk3b63vm95iv3hrx1yil68iz2cwhn86nkq";
+    rev = "3c9faef1212d5e692dc196348b229056ad1432ba";
+    sha256 = "1k7zq285adcr5w0rj1rw71kyc2nrrzqggss2z54r5zfvzcah25ab";
   };
 
   holofuel = fetchurl {
